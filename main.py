ffb65c14e308a1d9b43b62385a3b0f80076dd39d
Liveranski, Matheus [ITAS] <mdasil31@its.jnj.com>
2022-03-30 20:48:15 -0300

    Added treatment to initialize_motoboys_from_data to only initialize selected motoboys

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from classes.motoboy_class import motoboy
 from classes.pedidos_class import pedidos
 from classes.lojas_class import lojas
 
-def initialize_motoboys_from_data(motoboys_data):
+def initialize_motoboys_from_data(motoboys_data, selected_motoboy_ids):
     return [
         motoboy(
             motoboy_data.get("id"), 
             motoboy_data.get("stores"), 
             motoboy_data.get("fare_price")
         ) for motoboy_data in motoboys_data
+          if motoboy_data.get("id") in selected_motoboy_ids
     ]
 
 def initialize_lojas_from_data(lojas_data):
@@ -34,13 +35,14 @@ def initialize_class_data_by_motoboy_ids(motoboy_ids = None, data = dados):
     motoboys_data_list = data.motoboys
     lojas_data_list = data.lojas
     pedidos_data_list = data.pedidos
-    motoboys_list = initialize_motoboys_from_data(motoboys_data_list)
+    motoboys_list = initialize_motoboys_from_data(motoboys_data_list, motoboy_ids)
     lojas_list = initialize_lojas_from_data(lojas_data_list)
     pedidos_list = initialize_pedidos_from_data(pedidos_data_list)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         motoboy_ids = sys.argv[1].split(",")
+        motoboy_ids = [int(motoboy_id) for motoboy_id in motoboy_ids]
         initialize_class_data_by_motoboy_ids(motoboy_ids)
     else:
         initialize_class_data_by_motoboy_ids()
