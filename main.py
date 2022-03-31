e836af1c7c765766b3d50b40d4ea9cf3235ac045
Liveranski, Matheus [ITAS] <mdasil31@its.jnj.com>
2022-03-31 20:46:36 -0300

    Changed treatment for multiple motoboys received in sys.argv Created utility functions get_loja_comission_by_loja_id and get_loja_ids_by_pedido_ids Finished distribute_pedidos logic Created calculate_motoboy_profit function to return motoboy profit based on assigned orders, motoboy fare and orders' store comission Created display_motoboys_profit to format and display desired motoboy data after assigned orders

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import sys
+from math import ceil
 import data.dict_data as dados
 from classes.motoboy_class import motoboy
 from classes.pedidos_class import pedido
 from classes.lojas_class import loja
 
-def initialize_motoboys_from_data(motoboys_data, selected_motoboy_ids = None):
+def initialize_motoboys_from_data(motoboys_data):
     return [
         motoboy(
             motoboy_data.get("id"), 
             motoboy_data.get("exclusive_stores"), 
             motoboy_data.get("fare_price")
         ) for motoboy_data in motoboys_data
-          if selected_motoboy_ids and motoboy_data.get("id") in selected_motoboy_ids
-          or not selected_motoboy_ids
     ]
 
 def initialize_lojas_from_data(lojas_data):
@@ -32,40 +31,80 @@ def initialize_pedidos_from_data(pedidos_data):
         ) for pedido_data in pedidos_data
     ]
 
-def initialize_class_data_by_motoboy_ids(motoboy_ids = None, data = dados):
+def initialize_class_data(data = dados):
     motoboys_data_list = data.motoboys
     lojas_data_list = data.lojas
     pedidos_data_list = data.pedidos
-    motoboys_list = initialize_motoboys_from_data(motoboys_data_list, motoboy_ids)
+    motoboys_list = initialize_motoboys_from_data(motoboys_data_list)
     lojas_list = initialize_lojas_from_data(lojas_data_list)
     pedidos_list = initialize_pedidos_from_data(pedidos_data_list)
 
     return motoboys_list, lojas_list, pedidos_list
 
+
+
+def get_loja_comission_by_loja_id(lojas_list, id_loja):
+    loja_comission = [loja.comission for loja in lojas_list if loja.id == id_loja]
+    if loja_comission:
+        return loja_comission[0]
+    else:
+        raise ValueError('ID Loja {id} not found.'.format(id=id_loja))
+
+def get_loja_ids_by_pedido_ids(lojas_list, pedidos_list, pedido_ids):
+    pedido_ids = [pedido.id_loja for pedido in pedidos_list if pedido.id in pedido_ids]
+    loja_ids = [loja.id for loja in lojas_list if loja.id in pedido_ids]
+
+    return loja_ids
+
 def distribute_pedidos(motoboys_list, pedidos_list):
-    dict_id_motoboy_and_id_lojas_exclusiva = [
-        {
-            "id_motoboy": motoboy.id,
-            "id_lojas_exclusiva": motoboy.exclusive_stores
-        } for motoboy in motoboys_list 
+    [id_lojas_com_motoboy_exclusivo] = [
+        motoboy.exclusive_stores for motoboy in motoboys_list 
         if motoboy.exclusive_stores
     ]
 
-    assigned_orders_ids = []
+    avg_orders_per_motoboy = ceil(len(pedidos_list) / len(motoboys_list))
+    
+    for motoboy in motoboys_list:
+        motoboy.assign_orders(pedidos_list, id_lojas_com_motoboy_exclusivo, avg_orders_per_motoboy)
+        
 
-    for pedido in pedidos_list:
-        for motoboy in motoboys_list:
-            motoboy.assigned_orders.append(pedido.id)
-            assigned_orders_ids.append(pedido.id)
+def calculate_motoboy_profit(motoboys_list, lojas_list, pedidos_list):
+    for motoboy in motoboys_list:
+        motoboy_assigned_pedidos_list = [
+            pedido for pedido in pedidos_list if pedido.id in motoboy.assigned_orders
+        ]
+
+        for assigned_pedido in motoboy_assigned_pedidos_list:
+            loja_comission = get_loja_comission_by_loja_id(lojas_list, assigned_pedido.id_loja)
+            motoboy_profit = (assigned_pedido.price * loja_comission) + motoboy.fare_price
+
+            motoboy.profit += motoboy_profit
+
+def display_motoboys_profit(motoboys_list, motoboy_ids=None):
+    filtered_motoboys_list = [
+        motoboy for motoboy in motoboys_list if not motoboy_ids or motoboy.id in motoboy_ids
+        ]
+    for motoboy in filtered_motoboys_list:
+        print("Motoboy {id_motoboy}:".format(id_motoboy=motoboy.id))
+
+        formatted_pedido_ids = ", ".join([str(id) for id in motoboy.assigned_orders])
+        print("Terá {qty_pedidos} pedidos.".format(qty_pedidos=len(motoboy.assigned_orders)))
+        print("Ele entregará o(s) pedido(s) {display_pedido_ids}.".format(display_pedido_ids=formatted_pedido_ids))
+        print("O motoboy será pago R${profit:.2f}".format(profit=motoboy.profit))
+
+        loja_ids = get_loja_ids_by_pedido_ids(lojas_list, pedidos_list, motoboy.assigned_orders)
+        loja_ids_formatted = ", ".join([str(num) for num in loja_ids])
+        print("Os pedidos serão da(s) loja(s) {display_loja_ids}.\n".format(display_loja_ids=loja_ids_formatted))
 
 if __name__ == "__main__":
+    motoboy_ids = None
     if len(sys.argv) > 1:
         motoboy_ids = sys.argv[1].split(",")
         motoboy_ids = list(map(int, motoboy_ids))
-        motoboys_list, lojas_list, pedidos_list = initialize_class_data_by_motoboy_ids(
-            motoboy_ids
-        )
-    else:
-        motoboys_list, lojas_list, pedidos_list = initialize_class_data_by_motoboy_ids()
-        distribute_pedidos(motoboys_list, pedidos_list)
+        
+    motoboys_list, lojas_list, pedidos_list = initialize_class_data()
+        
+    distribute_pedidos(motoboys_list, pedidos_list)
+    calculate_motoboy_profit(motoboys_list, lojas_list, pedidos_list)
+    display_motoboys_profit(motoboys_list, motoboy_ids)
     
